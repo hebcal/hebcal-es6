7a2a585300b14bddce8cbfd5a33553f92978bff9
Michael J. Radwin <michael@radwin.org>
2020-04-12 09:36:55 -0700

    Initial checkin of ES6 module

---
+++ index.js
@@ -0,0 +1,6 @@
+import * as greg from './src/greg.mjs';
+
+console.log("hello");
+let foo = greg.daysInMonth(2, 2020);
+console.log(foo);
+console.log("goodbye");
---
+++ src/greg.mjs
@@ -0,0 +1,112 @@
+"use strict";
+/*
+    Hebcal - A Jewish Calendar Generator
+    Copyright (C) 1994-2004  Danny Sadinoff
+    Portions Copyright (c) 2002 Michael J. Radwin. All Rights Reserved.
+
+    https://github.com/hebcal/hebcal-es6
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+    Danny Sadinoff can be reached at danny@sadinoff.com
+
+    Michael Radwin has made significant contributions as a result of
+    maintaining hebcal.com.
+
+    The JavaScript code was completely rewritten in 2014 by Eyal Schachter.
+ */
+const monthLengths = [
+    [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+    [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+];
+
+export function LEAP(year) {
+    return !(year % 4) && ( !!(year % 100) || !(year % 400) );
+}
+
+export function daysInMonth(month, year) { // 1 based months
+    return monthLengths[+LEAP(year)][month];
+}
+
+export const monthNames = [
+    '',
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+export function lookupMonthNum(month) {
+    return new Date(month + ' 1').getMonth() + 1;
+}
+
+export function dayOfYear(date) {
+    if (!date instanceof Date) {
+        throw new TypeError('Argument to greg.dayOfYear not a Date');
+    }
+    let doy = date.getDate() + 31 * date.getMonth();
+    if (date.getMonth() > 1) { // FEB
+        doy -= Math.floor((4 * (date.getMonth() + 1) + 23) / 10);
+        if (LEAP(date.getFullYear())) {
+            doy++;
+        }
+    }
+    return doy;
+}
+
+export function greg2abs(date) { // "absolute date"
+    const year = date.getFullYear() - 1;
+    return (dayOfYear(date) + // days this year
+            365 * year + // + days in prior years
+            ( Math.floor(year / 4) - // + Julian Leap years
+            Math.floor(year / 100) + // - century years
+            Math.floor(year / 400))); // + Gregorian leap years
+}
+
+/*
+ * See the footnote on page 384 of ``Calendrical Calculations, Part II:
+ * Three Historical Calendars'' by E. M. Reingold,  N. Dershowitz, and S. M.
+ * Clamen, Software--Practice and Experience, Volume 23, Number 4
+ * (April, 1993), pages 383-404 for an explanation.
+ */
+export function abs2greg(theDate) {
+// calculations copied from original JS code
+    const d0 = theDate - 1;
+    const n400 = Math.floor(d0 / 146097);
+    const d1 =  Math.floor(d0 % 146097);
+    const n100 =  Math.floor(d1 / 36524);
+    const d2 = d1 % 36524;
+    const n4 =  Math.floor(d2 / 1461);
+    const d3 = d2 % 1461;
+    const n1 =  Math.floor(d3 / 365);
+
+    const day = ((d3 % 365) + 1);
+    let year = (400 * n400 + 100 * n100 + 4 * n4 + n1);
+
+    if (4 == n100 || 4 == n1) {
+        return new Date(year, 11, 31);
+    }
+
+    return new Date(new Date(++year, 0, day).setFullYear(year)); // new Date() is very smart
+}
+
+export default abs2greg;
