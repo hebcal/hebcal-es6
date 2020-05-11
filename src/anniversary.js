f826947d24a7e562aa826e9bd5c6c0284d087779
Michael J. Radwin <michael@radwin.org>
2020-05-11 15:10:05 -0700

    Add unit test for yahrzeit

--- src/anniversary.js
+++ src/anniversary.js
@@ -1,3 +1,23 @@
+/*
+    Hebcal - A Jewish Calendar Generator
+    Copyright (c) 1994-2020 Danny Sadinoff
+    Portions copyright Eyal Schachter and Michael J. Radwin
+
+    https://github.com/hebcal/hebcal-es6
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 import common from "./common";
 import { HDate, hebrew2abs, abs2hebrew } from './hdate';
 import { greg2abs } from './greg';
