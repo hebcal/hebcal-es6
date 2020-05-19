9e99ba144e40af4bda2a1404cf397953888faed9
Michael J. Radwin <michael@radwin.org>
2020-05-19 08:45:48 -0700

    Use sedraArray for efficiency

--- src/calc-triennial.js
+++ src/calc-triennial.js
@@ -1,5 +1,6 @@
 import minimist from 'minimist';
-import hebcal, { HDate } from './hebcal';
+import { HDate } from './hdate';
+import { Sedra, parshiot } from './sedra';
 
 const args = minimist(process.argv.slice(2), {
     alias: {
@@ -10,21 +11,28 @@ const args = minimist(process.argv.slice(2), {
 const opt_israel = false;
 //console.log('args:', args);
 
-
-const parshiyot = require('./aliyot.json');
+const parshiyotObj = require('./aliyot.json');
 const festivals = require('./holiday-readings.json');
 
 const triennialAliyot = {};
 const triennialAliyotAlt = {};
-read_aliyot_metadata(parshiyot);
+read_aliyot_metadata(parshiyotObj);
 
-const allInOrder = [];
-const combined = [];
-const combinedMap = {};
-const parashah2id = {};
-const prev = {};
-const next = {};
-getOrderAndPrevNext(parshiyot);
+const allInOrder = parshiot.slice();
+const doubled = [
+    21, // Vayakhel-Pekudei
+    26, // Tazria-Metzora
+    28, // Achrei Mot-Kedoshim
+    31, // Behar-Bechukotai
+    38, // Chukat-Balak
+    41, // Matot-Masei
+    50  // Nitzavim-Vayeilech
+];
+const isSometimesDoubled = {};
+doubled.forEach((id) => {
+    isSometimesDoubled[id] = true;
+    isSometimesDoubled[id + 1] = true;
+});
 
 // year I in triennial cycle was 5756
 const today = new HDate();
@@ -34,125 +42,99 @@ console.log(`Current Hebrew year ${hebrew_year} is year ${year_num}.`);
 
 const cycle_start_years = [];
 const triennial_readings = [];
-const triennial_events = [];
-const bereshit_indices = [];
 const max_triennial_cycles = 5;
 for (let i = 0; i < max_triennial_cycles; i++) {
     const year_offset = (i - 1) * 3;
     const cycle_start_year = hebrew_year - (year_num - 1) + year_offset;
     cycle_start_years[i] = cycle_start_year;
     console.log(`3-cycle started at year ${cycle_start_year}`);
-    const [bereshit_idx,pattern,events] = get_tri_events(cycle_start_year);
-    triennial_events[i] = events;
-    bereshit_indices[i] = bereshit_idx;
     if (!opt_israel) {
-        const cycle_option = calc_variation_options(parshiyot,pattern);
-        triennial_readings[i] = cycle_readings(triennialAliyot,bereshit_idx,events,cycle_option);
-    //	triennial_alt_readings[i] = cycle_readings(\%triennial_aliyot_alt,bereshit_idx,events,cycle_option);
-    }
-}
-
-function get_tri_events(start) {
-    const options = {
-        year: start,
-        isHebrewYear: true,
-        numYears: 3,
-        noHolidays: true,
-        sedrot: true,
-        il: opt_israel
-    };
-    const events = hebcal.hebcalEvents(options);
-    let idx = -1;
-    for (let i = 0; i < events.length; i++) {
-///        console.log(i, events[i]);
-        if (events[i].getDesc() == 'Parashat Bereshit') {
-//            console.log(`***** found bereshit ${i}`);
-            idx = i;
-            break;
-        }
-    }
-    if (idx == -1) {
-        throw new Error("can't find Bereshit for Year I");
-    }
-    // determine triennial year patterns
-    const pattern = {};
-    for (const ev of events) {
-        const subj = ev.getDesc().substring(9); // skip "Parashat "
-        if (subj.indexOf('-') != -1) {
-            const [p1,p2] = subj.split('-');
-            pattern[p1] = pattern[p1] || [];
-            pattern[p2] = pattern[p2] || [];
-            pattern[p1].push('T');
-            pattern[p2].push('T');
-        } else {
-            pattern[subj] = pattern[subj] || [];
-            pattern[subj].push('S');
-        }
-    }
-    return [idx, pattern, events];
-}
-
-function calc_variation_options(parshiyot, pattern) {
-    const option = {};
-    for (const parsha of combined) {
-        const [p1,p2] = parsha.split('-');
-        let pat = '';
+        const sedras = [];
         for (const yr of [0,1,2]) {
-            pat += pattern[p1][yr];
+            sedras.push(new Sedra(cycle_start_year + yr, false));
         }
-        if (pat == 'TTT') {
-            option[parsha] = 'all-together';
-        } else {
-            const cycleOpt = parshiyot[parsha].triennial.patterns[pat];
-            if (typeof cycleOpt === 'undefined') {
-                throw new Error(`can't find option for ${parsha} (pat == ${pat}`);
-            }
-            option[parsha] = cycleOpt;
-            option[p1] = cycleOpt;
-            option[p2] = cycleOpt;
-        }
-        console.log(`  ${parsha} ${pat} (${option[parsha]})`);
+        let cycle_option = calcVariationOptions2(sedras);    
+        triennial_readings[i] = cycleReadings2(triennialAliyot, sedras, cycle_option);
+//        console.log(foo);
+
+        //	triennial_alt_readings[i] = cycle_readings(\%triennial_aliyot_alt,bereshit_idx,events,cycle_option);
     }
+}
+
+function getDoubledName(id) {
+    const p1 = parshiot[id];
+    const p2 = parshiot[id + 1];
+    const name = p1 + '-' + p2;
+    return name;
+}
+
+function calcVariationOptions2(sedras) {
+    const option = {};
+    for (const id of doubled) {
+        // First, determine if a doubled parsha is read [T]ogether or [S]eparately
+        // in each of the 3 years. Yields a pattern like 'SSS', 'STS', 'TTT', 'TTS'.
+        const p1 = parshiot[id];
+        const p2 = parshiot[id + 1];
+        const name = p1 + '-' + p2;
+        let pattern = '';
+        for (const yr of [0,1,2]) {
+            const sedraArray = sedras[yr].getSedraArray();
+            const pat = sedraArray.indexOf(-1 * id) == -1 ? 'S' : 'T';
+            pattern += pat;
+        }
+
+        // Next, look up the pattern in JSON to determine readings for each year
+        if (pattern == 'TTT') {
+            option[name] = 'all-together';
+        } else {
+            const variation = parshiyotObj[name].triennial.patterns[pattern];
+            if (typeof variation === 'undefined') {
+                throw new Error(`can't find option for ${name} (pat == ${pattern}`);
+            }
+            option[name] = option[p1] = option[p2] = variation;
+        }
+        console.log(`  ${name} ${pattern} (${option[name]})`);
+    }
+
     return option;
 }
 
-function cycle_readings(triennialAliyot, bereshit_idx, events, option) {
+function cycleReadings2(triennialAliyot, sedras, option) {
     const readings = {};
-    let yr = 1;
-    for (let i = bereshit_idx; i < events.length; i++) {
-        const subj = events[i].getDesc();
-        if (subj == 'Parashat Bereshit' && i != bereshit_idx) {
-            yr++;
-            if (yr == 4) {
-                break;
+    for (const parsha of parshiot) {
+        readings[parsha] = [];
+    }
+    for (const id of doubled) {
+        const parsha = getDoubledName(id);
+        readings[parsha] = [];
+    }
+    for (const yr of [1,2,3]) {
+        const sedraArray = sedras[yr-1].getSedraArray();
+        const bereshit_idx = sedraArray.indexOf(0); // parshiot[0]='Bereshit'
+        for (let i = bereshit_idx; i < sedraArray.length; i++) {
+            const id = sedraArray[i];
+            if (typeof id === 'number') {
+                const h = (id < 0) ? getDoubledName(-id) : parshiot[id];
+                if (isSometimesDoubled[id]) {
+                    const variation = option[h] + "." + yr;
+                    const a         = triennialAliyot[h][variation];
+                    if (!a) {
+                        throw new Error(`can't find ${h} year ${yr} (variation ${variation})`);
+                    }
+                    readings[h][yr] = a;
+                } else if (triennialAliyot[h][`Y.${yr}`]) {
+                    const a = triennialAliyot[h][`Y.${yr}`];
+                    readings[h][yr] = a;
+                } else {
+                    throw new Error(`can't find aliyot for ${h}, year ${yr}`);
+                }
             }
-        }
-        const h = subj.substring(9);
-//        console.log(`${h} year ${yr} (idx=${i})`);
-        readings[h] = readings[h] || [];
-        if (combinedMap[h]) {
-            const variation = option[h] + "." + yr;
-            const a         = triennialAliyot[h][variation];
-            if (!a) {
-                throw new Error();
-            }
-            readings[h][yr] = [ a, events[i], h ];
-        } else if (triennialAliyot[h][`Y.${yr}`]) {
-            const a = triennialAliyot[h][`Y.${yr}`];
-            readings[h][yr] = [ a, events[i], h ];
-            if (h.indexOf('-') != -1) {
-                const [p1,p2] = h.split('-');
-                readings[p1] = readings[p1] || [];
-                readings[p2] = readings[p2] || [];
-                readings[p1][yr] = [ a, events[i], h ];
-                readings[p2][yr] = [ a, events[i], h ];
-            }
-        } else {
-            throw new Error(`can't find aliyot for ${h}, year ${yr}`);
         }
     }
+
     return readings;
 }
+
 
 function read_aliyot_metadata(parshiyot) {
     // build a lookup table so we don't have to follow num/variation/sameas
@@ -189,31 +171,5 @@ function readAliyotMetadataInner(parsha, triennial, lookup) {
             }
             lookup[parsha][num] = lookup[parsha][aliyot];
         }
-    }
-}
-
-
-function getOrderAndPrevNext(parshiyot) {
-    for (const [parsha, value] of Object.entries(parshiyot)) {
-        const num = value.num;
-        if (value.combined) {
-            combined[num - 101] = parsha;
-            const [p1,p2] = parsha.split('-');
-            combinedMap[p1] = parsha;
-            combinedMap[p2] = parsha;
-        } else {
-            allInOrder[num - 1] = parsha;
-            parashah2id[parsha] = num;
-        }
-    }
-    let h2 = undefined;
-    for (const h of allInOrder) {
-        prev[h] = h2;
-        h2 = h;
-    }
-    h2 = undefined;
-    for (const h of allInOrder.slice().reverse()) {
-        next[h] = h2;
-        h2 = h;
     }
 }
