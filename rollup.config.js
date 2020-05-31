0535fb6812762d0ed456f43c17531028af75d3c7
Michael J. Radwin <michael@radwin.org>
2020-05-31 09:45:20 -0700

    Rollup tweaks

--- rollup.config.js
+++ rollup.config.js
@@ -8,46 +8,24 @@ export default [
   // browser-friendly UMD build
   {
     input: 'src/index.js',
-    output: [{
-      name: 'hebcal',
-      file: pkg.browser,
-      format: 'umd',
-    },
-    {
-      file: 'dist/bundle.min.js',
-      format: 'umd',
-      name: 'hebcal',
-      plugins: [terser()],
-
-    }],
-    plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      babel({
-        babelHelpers: 'bundled',
-        exclude: ['node_modules/**'],
-      }),
-    ],
-  },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: 'src/index.js',
-    external: ['ttag', 'numeral', 'suncalc'],
     output: [
+      {file: pkg.browser, format: 'umd', name: 'hebcal-core'},
       {file: pkg.main, format: 'cjs'},
       {file: pkg.module, format: 'es'},
+      {
+        file: 'dist/bundle.min.js',
+        format: 'umd',
+        name: 'hebcal-leyning',
+        plugins: [terser()],
+      },
     ],
     plugins: [
       babel({
         babelHelpers: 'bundled',
         exclude: ['node_modules/**'],
       }),
+      resolve(),
+      commonjs(),
     ],
   },
 ];
