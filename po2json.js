70d09b6e46f0a8db1b62008fab141878aed7177b
Michael J. Radwin <michael@radwin.org>
2020-06-09 12:13:46 -0700

    More efficient po2json

---
+++ po2json.js
@@ -0,0 +1,28 @@
+/* eslint-disable require-jsdoc */
+const fs = require('fs');
+const path = require('path');
+const parser = require('ttag-cli/dist/src/lib/parser');
+const utils = require('ttag-cli/dist/src/lib/utils');
+
+for (const arg of process.argv.slice(2)) {
+  const outpath = './src/' + path.basename(arg) + '.po';
+  console.log(`${arg} => ${outpath}`);
+  writePoFile(arg, outpath, false);
+}
+
+function writePoFile(inpath, outpath, nostrip) {
+  const poData = parser.parse(
+      fs.readFileSync(inpath).toString(),
+  );
+  const messages = utils.iterateTranslations(poData.translations);
+  if (!nostrip) {
+    const header = messages.next().value;
+    delete header.comments;
+    for (const msg of messages) {
+      delete msg.comments;
+    }
+  }
+  const outstream = fs.createWriteStream('out.json', {flags: 'w'});
+  outstream.write(JSON.stringify(utils.convert2Compact(poData), null, 0));
+  outstream.end();
+}
