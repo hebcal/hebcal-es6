5baee4a5d6acbd1a94941c2ac9d631f57cbc585b
Michael J. Radwin <michael@radwin.org>
2020-07-12 09:16:52 -0700

    Rewrite test to use deepEqual

--- src/holidays.spec.js
+++ src/holidays.spec.js
@@ -72,56 +72,27 @@ test('shushan-purim', (t) => {
  * @param {Object} t
  * @param {number} hyear
  * @param {boolean} il
- * @param {string[]} expected0
+ * @param {string[]} expected
  * @private
  */
-function testFullYear(t, hyear, il, expected0) {
-  const expected = {};
-  for (const line of expected0) {
-    const space = line.indexOf(' ');
-    const dt = line.substring(0, space);
-    const desc = line.substring(space + 1);
-    if (expected[dt]) {
-      expected[dt].push(desc);
-    } else {
-      expected[dt] = [desc];
-    }
-  }
+function testFullYear(t, hyear, il, expected) {
   const year = HebrewCalendar.getHolidaysForYear(hyear);
   const startAbs = HDate.hebrew2abs(hyear, months.TISHREI, 1);
   const endAbs = HDate.hebrew2abs(hyear + 1, months.TISHREI, 1);
+  let actual = [];
   for (let absDt = startAbs; absDt <= endAbs; absDt++) {
     const hebDt = new HDate(absDt);
     const gregDt = g.abs2greg(absDt);
-    const dateStr = gregDt.toLocaleDateString('en-US');
-    const ev = year.get(hebDt.toString());
-    if (typeof ev !== 'undefined') {
-      const evFiltered = ev.filter((e) => !(e.getFlags() & flags.SHABBAT_MEVARCHIM));
-      for (const e of evFiltered) {
-        if ((il && e.observedInIsrael()) || (!il && e.observedInDiaspora())) {
-          const desc = e.getDesc();
-          if (expected[dateStr]) {
-            let found = false;
-            for (const h of expected[dateStr]) {
-              if (h === desc) {
-                found = true;
-                t.is(desc, h);
-              }
-            }
-            if (!found) {
-              const j = expected[dateStr].join(',');
-              t.fail(`Found "${desc}" on ${dateStr}, but we expected {${j}}`);
-            }
-          } else {
-            t.fail(`Unexpected ${dateStr} ${desc}`);
-          }
-        }
-      }
-    } else if (expected[dateStr]) {
-      const j = expected[dateStr].join(',');
-      t.fail(`No events found on ${dateStr}, but we expected {${j}}`);
-    }
+    const dateStr = gregDt.toISOString().substring(0, 10);
+    const evRaw = year.get(hebDt.toString()) || [];
+    const events = evRaw
+        .filter((e) => !(e.getFlags() & flags.SHABBAT_MEVARCHIM))
+        .filter((e) => (il && e.observedInIsrael()) || (!il && e.observedInDiaspora()));
+    actual = actual.concat(events.map((e) => dateStr + ' ' + e.getDesc()));
   }
+  t.is(actual.length, expected.length);
+  actual.sort();
+  t.deepEqual(actual, expected);
 }
 
 test('diaspora', (t) => {
@@ -134,178 +105,174 @@ test('israel', (t) => {
   testFullYear(t, 5720, true, expected);
 });
 
-// hebcal -i -H -5720
+// hebcal -g -i -H 5720
 // eslint-disable-next-line require-jsdoc
 function israel5720() {
-  return `10/2/1959 Erev Rosh Hashana
-10/3/1959 Rosh Hashana 5720
-10/4/1959 Rosh Hashana II
-10/5/1959 Tzom Gedaliah
-10/10/1959 Shabbat Shuva
-10/11/1959 Erev Yom Kippur
-10/12/1959 Yom Kippur
-10/16/1959 Erev Sukkot
-10/17/1959 Sukkot I
-10/18/1959 Sukkot II (CH''M)
-10/19/1959 Sukkot III (CH''M)
-10/20/1959 Sukkot IV (CH''M)
-10/21/1959 Sukkot V (CH''M)
-10/22/1959 Sukkot VI (CH''M)
-10/23/1959 Sukkot VII (Hoshana Raba)
-10/24/1959 Shmini Atzeret
-11/1/1959 Rosh Chodesh Cheshvan
-11/2/1959 Rosh Chodesh Cheshvan
-12/1/1959 Rosh Chodesh Kislev
-12/2/1959 Rosh Chodesh Kislev
-12/25/1959 Chanukah: 1 Candle
-12/26/1959 Chanukah: 2 Candles
-12/27/1959 Chanukah: 3 Candles
-12/28/1959 Chanukah: 4 Candles
-12/29/1959 Chanukah: 5 Candles
-12/30/1959 Chanukah: 6 Candles
-12/31/1959 Rosh Chodesh Tevet
-12/31/1959 Chanukah: 7 Candles
-1/1/1960 Rosh Chodesh Tevet
-1/1/1960 Chanukah: 8 Candles
-1/2/1960 Chanukah: 8th Day
-1/10/1960 Asara B'Tevet
-1/30/1960 Rosh Chodesh Sh'vat
-2/13/1960 Tu BiShvat
-2/27/1960 Shabbat Shekalim
-2/28/1960 Rosh Chodesh Adar
-2/29/1960 Rosh Chodesh Adar
-3/10/1960 Ta'anit Esther
-3/12/1960 Shabbat Zachor
-3/12/1960 Erev Purim
-3/13/1960 Purim
-3/14/1960 Shushan Purim
-3/19/1960 Shabbat Parah
-3/26/1960 Shabbat HaChodesh
-3/29/1960 Rosh Chodesh Nisan
-4/9/1960 Shabbat HaGadol
-4/11/1960 Ta'anit Bechorot
-4/11/1960 Erev Pesach
-4/12/1960 Pesach I
-4/13/1960 Pesach II (CH''M)
-4/14/1960 Pesach III (CH''M)
-4/15/1960 Pesach IV (CH''M)
-4/16/1960 Pesach V (CH''M)
-4/17/1960 Pesach VI (CH''M)
-4/18/1960 Pesach VII
-4/25/1960 Yom HaShoah
-4/27/1960 Rosh Chodesh Iyyar
-4/28/1960 Rosh Chodesh Iyyar
-5/1/1960 Yom HaZikaron
-5/2/1960 Yom HaAtzma'ut
-5/11/1960 Pesach Sheni
-5/15/1960 Lag BaOmer
-5/27/1960 Rosh Chodesh Sivan
-5/31/1960 Erev Shavuot
-6/1/1960 Shavuot
-6/25/1960 Rosh Chodesh Tamuz
-6/26/1960 Rosh Chodesh Tamuz
-7/12/1960 Tzom Tammuz
-7/25/1960 Rosh Chodesh Av
-7/30/1960 Shabbat Chazon
-8/1/1960 Erev Tish'a B'Av
-8/2/1960 Tish'a B'Av
-8/6/1960 Shabbat Nachamu
-8/8/1960 Tu B'Av
-8/23/1960 Rosh Chodesh Elul
-8/24/1960 Rosh Chodesh Elul
-9/17/1960 Leil Selichot
-9/21/1960 Erev Rosh Hashana
-`.split('\n');
+  return `1959-10-03 Rosh Hashana 5720
+1959-10-04 Rosh Hashana II
+1959-10-05 Tzom Gedaliah
+1959-10-10 Shabbat Shuva
+1959-10-11 Erev Yom Kippur
+1959-10-12 Yom Kippur
+1959-10-16 Erev Sukkot
+1959-10-17 Sukkot I
+1959-10-18 Sukkot II (CH''M)
+1959-10-19 Sukkot III (CH''M)
+1959-10-20 Sukkot IV (CH''M)
+1959-10-21 Sukkot V (CH''M)
+1959-10-22 Sukkot VI (CH''M)
+1959-10-23 Sukkot VII (Hoshana Raba)
+1959-10-24 Shmini Atzeret
+1959-11-01 Rosh Chodesh Cheshvan
+1959-11-02 Rosh Chodesh Cheshvan
+1959-12-01 Rosh Chodesh Kislev
+1959-12-02 Rosh Chodesh Kislev
+1959-12-25 Chanukah: 1 Candle
+1959-12-26 Chanukah: 2 Candles
+1959-12-27 Chanukah: 3 Candles
+1959-12-28 Chanukah: 4 Candles
+1959-12-29 Chanukah: 5 Candles
+1959-12-30 Chanukah: 6 Candles
+1959-12-31 Rosh Chodesh Tevet
+1959-12-31 Chanukah: 7 Candles
+1960-01-01 Rosh Chodesh Tevet
+1960-01-01 Chanukah: 8 Candles
+1960-01-02 Chanukah: 8th Day
+1960-01-10 Asara B'Tevet
+1960-01-30 Rosh Chodesh Sh'vat
+1960-02-13 Tu BiShvat
+1960-02-27 Shabbat Shekalim
+1960-02-28 Rosh Chodesh Adar
+1960-02-29 Rosh Chodesh Adar
+1960-03-10 Ta'anit Esther
+1960-03-12 Shabbat Zachor
+1960-03-12 Erev Purim
+1960-03-13 Purim
+1960-03-14 Shushan Purim
+1960-03-19 Shabbat Parah
+1960-03-26 Shabbat HaChodesh
+1960-03-29 Rosh Chodesh Nisan
+1960-04-09 Shabbat HaGadol
+1960-04-11 Ta'anit Bechorot
+1960-04-11 Erev Pesach
+1960-04-12 Pesach I
+1960-04-13 Pesach II (CH''M)
+1960-04-14 Pesach III (CH''M)
+1960-04-15 Pesach IV (CH''M)
+1960-04-16 Pesach V (CH''M)
+1960-04-17 Pesach VI (CH''M)
+1960-04-18 Pesach VII
+1960-04-25 Yom HaShoah
+1960-04-27 Rosh Chodesh Iyyar
+1960-04-28 Rosh Chodesh Iyyar
+1960-05-01 Yom HaZikaron
+1960-05-02 Yom HaAtzma'ut
+1960-05-11 Pesach Sheni
+1960-05-15 Lag BaOmer
+1960-05-27 Rosh Chodesh Sivan
+1960-05-31 Erev Shavuot
+1960-06-01 Shavuot
+1960-06-25 Rosh Chodesh Tamuz
+1960-06-26 Rosh Chodesh Tamuz
+1960-07-12 Tzom Tammuz
+1960-07-25 Rosh Chodesh Av
+1960-07-30 Shabbat Chazon
+1960-08-01 Erev Tish'a B'Av
+1960-08-02 Tish'a B'Av
+1960-08-06 Shabbat Nachamu
+1960-08-08 Tu B'Av
+1960-08-23 Rosh Chodesh Elul
+1960-08-24 Rosh Chodesh Elul
+1960-09-17 Leil Selichot
+1960-09-21 Erev Rosh Hashana`.split('\n').sort();
 }
 
 // hebcal -H 5771
 // eslint-disable-next-line require-jsdoc
 function diaspora5771() {
-  return `9/9/2010 Rosh Hashana 5771
-9/10/2010 Rosh Hashana II
-9/11/2010 Shabbat Shuva
-9/12/2010 Tzom Gedaliah
-9/17/2010 Erev Yom Kippur
-9/18/2010 Yom Kippur
-9/22/2010 Erev Sukkot
-9/23/2010 Sukkot I
-9/24/2010 Sukkot II
-9/25/2010 Sukkot III (CH''M)
-9/26/2010 Sukkot IV (CH''M)
-9/27/2010 Sukkot V (CH''M)
-9/28/2010 Sukkot VI (CH''M)
-9/29/2010 Sukkot VII (Hoshana Raba)
-9/30/2010 Shmini Atzeret
-10/1/2010 Simchat Torah
-10/8/2010 Rosh Chodesh Cheshvan
-10/9/2010 Rosh Chodesh Cheshvan
-11/6/2010 Sigd
-11/7/2010 Rosh Chodesh Kislev
-11/8/2010 Rosh Chodesh Kislev
-12/1/2010 Chanukah: 1 Candle
-12/2/2010 Chanukah: 2 Candles
-12/3/2010 Chanukah: 3 Candles
-12/4/2010 Chanukah: 4 Candles
-12/5/2010 Chanukah: 5 Candles
-12/6/2010 Chanukah: 6 Candles
-12/7/2010 Rosh Chodesh Tevet
-12/7/2010 Chanukah: 7 Candles
-12/8/2010 Rosh Chodesh Tevet
-12/8/2010 Chanukah: 8 Candles
-12/9/2010 Chanukah: 8th Day
-12/17/2010 Asara B'Tevet
-1/6/2011 Rosh Chodesh Sh'vat
-1/20/2011 Tu BiShvat
-2/4/2011 Rosh Chodesh Adar I
-2/5/2011 Rosh Chodesh Adar I
-2/18/2011 Purim Katan
-3/5/2011 Shabbat Shekalim
-3/6/2011 Rosh Chodesh Adar II
-3/7/2011 Rosh Chodesh Adar II
-3/17/2011 Ta'anit Esther
-3/19/2011 Shabbat Zachor
-3/19/2011 Erev Purim
-3/20/2011 Purim
-3/21/2011 Shushan Purim
-3/26/2011 Shabbat Parah
-4/2/2011 Shabbat HaChodesh
-4/5/2011 Rosh Chodesh Nisan
-4/16/2011 Shabbat HaGadol
-4/18/2011 Ta'anit Bechorot
-4/18/2011 Erev Pesach
-4/19/2011 Pesach I
-4/20/2011 Pesach II
-4/20/2011 Hello****
-4/21/2011 Pesach III (CH''M)
-4/22/2011 Pesach IV (CH''M)
-4/23/2011 Pesach V (CH''M)
-4/24/2011 Pesach VI (CH''M)
-4/25/2011 Pesach VII
-4/26/2011 Pesach VIII
-5/2/2011 Yom HaShoah
-5/4/2011 Rosh Chodesh Iyyar
-5/5/2011 Rosh Chodesh Iyyar
-5/9/2011 Yom HaZikaron
-5/10/2011 Yom HaAtzma'ut
-5/18/2011 Pesach Sheni
-5/22/2011 Lag BaOmer
-6/1/2011 Yom Yerushalayim
-6/3/2011 Rosh Chodesh Sivan
-6/7/2011 Erev Shavuot
-6/8/2011 Shavuot I
-6/9/2011 Shavuot II
-7/2/2011 Rosh Chodesh Tamuz
-7/3/2011 Rosh Chodesh Tamuz
-7/19/2011 Tzom Tammuz
-8/1/2011 Rosh Chodesh Av
-8/6/2011 Shabbat Chazon
-8/8/2011 Erev Tish'a B'Av
-8/9/2011 Tish'a B'Av
-8/13/2011 Shabbat Nachamu
-8/15/2011 Tu B'Av
-8/30/2011 Rosh Chodesh Elul
-8/31/2011 Rosh Chodesh Elul
-9/24/2011 Leil Selichot
-9/28/2011 Erev Rosh Hashana
-`.split('\n');
+  return `2010-09-09 Rosh Hashana 5771
+2010-09-10 Rosh Hashana II
+2010-09-11 Shabbat Shuva
+2010-09-12 Tzom Gedaliah
+2010-09-17 Erev Yom Kippur
+2010-09-18 Yom Kippur
+2010-09-22 Erev Sukkot
+2010-09-23 Sukkot I
+2010-09-24 Sukkot II
+2010-09-25 Sukkot III (CH''M)
+2010-09-26 Sukkot IV (CH''M)
+2010-09-27 Sukkot V (CH''M)
+2010-09-28 Sukkot VI (CH''M)
+2010-09-29 Sukkot VII (Hoshana Raba)
+2010-09-30 Shmini Atzeret
+2010-10-01 Simchat Torah
+2010-10-08 Rosh Chodesh Cheshvan
+2010-10-09 Rosh Chodesh Cheshvan
+2010-11-06 Sigd
+2010-11-07 Rosh Chodesh Kislev
+2010-11-08 Rosh Chodesh Kislev
+2010-12-01 Chanukah: 1 Candle
+2010-12-02 Chanukah: 2 Candles
+2010-12-03 Chanukah: 3 Candles
+2010-12-04 Chanukah: 4 Candles
+2010-12-05 Chanukah: 5 Candles
+2010-12-06 Chanukah: 6 Candles
+2010-12-07 Rosh Chodesh Tevet
+2010-12-07 Chanukah: 7 Candles
+2010-12-08 Rosh Chodesh Tevet
+2010-12-08 Chanukah: 8 Candles
+2010-12-09 Chanukah: 8th Day
+2010-12-17 Asara B'Tevet
+2011-01-06 Rosh Chodesh Sh'vat
+2011-01-20 Tu BiShvat
+2011-02-04 Rosh Chodesh Adar I
+2011-02-05 Rosh Chodesh Adar I
+2011-02-18 Purim Katan
+2011-03-05 Shabbat Shekalim
+2011-03-06 Rosh Chodesh Adar II
+2011-03-07 Rosh Chodesh Adar II
+2011-03-17 Ta'anit Esther
+2011-03-19 Shabbat Zachor
+2011-03-19 Erev Purim
+2011-03-20 Purim
+2011-03-21 Shushan Purim
+2011-03-26 Shabbat Parah
+2011-04-02 Shabbat HaChodesh
+2011-04-05 Rosh Chodesh Nisan
+2011-04-16 Shabbat HaGadol
+2011-04-18 Ta'anit Bechorot
+2011-04-18 Erev Pesach
+2011-04-19 Pesach I
+2011-04-20 Pesach II
+2011-04-21 Pesach III (CH''M)
+2011-04-22 Pesach IV (CH''M)
+2011-04-23 Pesach V (CH''M)
+2011-04-24 Pesach VI (CH''M)
+2011-04-25 Pesach VII
+2011-04-26 Pesach VIII
+2011-05-02 Yom HaShoah
+2011-05-04 Rosh Chodesh Iyyar
+2011-05-05 Rosh Chodesh Iyyar
+2011-05-09 Yom HaZikaron
+2011-05-10 Yom HaAtzma'ut
+2011-05-18 Pesach Sheni
+2011-05-22 Lag BaOmer
+2011-06-01 Yom Yerushalayim
+2011-06-03 Rosh Chodesh Sivan
+2011-06-07 Erev Shavuot
+2011-06-08 Shavuot I
+2011-06-09 Shavuot II
+2011-07-02 Rosh Chodesh Tamuz
+2011-07-03 Rosh Chodesh Tamuz
+2011-07-19 Tzom Tammuz
+2011-08-01 Rosh Chodesh Av
+2011-08-06 Shabbat Chazon
+2011-08-08 Erev Tish'a B'Av
+2011-08-09 Tish'a B'Av
+2011-08-13 Shabbat Nachamu
+2011-08-15 Tu B'Av
+2011-08-30 Rosh Chodesh Elul
+2011-08-31 Rosh Chodesh Elul
+2011-09-24 Leil Selichot
+2011-09-28 Erev Rosh Hashana`.split('\n').sort();
 }
