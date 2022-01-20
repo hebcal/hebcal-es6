0893abdef5d62dc3b5c5937532657857f0a06d51
Michael J. Radwin <michael@radwin.org>
2022-01-20 14:28:39 -0800

    Add locale support for Mishna Yomi

--- src/MishnaYomiEvent.spec.js
+++ src/MishnaYomiEvent.spec.js
@@ -1,10 +1,21 @@
 import test from 'ava';
 import {MishnaYomiEvent} from './MishnaYomiEvent';
 import {HDate} from './hdate';
+import './locale-ashkenazi';
+import './locale-he';
 
 test('MishnaYomiEvent-url', (t) => {
   const my = [{k: 'Berakhot', v: '3:6'}, {k: 'Berakhot', v: '4:1'}];
   const hd = new HDate(new Date(1947, 4, 29));
   const ev = new MishnaYomiEvent(hd, my);
   t.is(ev.url(), 'https://www.sefaria.org/Mishnah_Berakhot.3.6-4.1?lang=bi');
+});
+
+test('MishnaYomiEvent-render', (t) => {
+  const my = [{k: 'Berakhot', v: '3:6'}, {k: 'Berakhot', v: '4:1'}];
+  const hd = new HDate(new Date(1947, 4, 29));
+  const ev = new MishnaYomiEvent(hd, my);
+  t.is(ev.render('en'), 'Berakhot 3:6-4:1');
+  t.is(ev.render('ashkenazi'), 'Berakhos 3:6-4:1');
+  t.is(ev.render('he'), 'ברכות 3:6-4:1');
 });
