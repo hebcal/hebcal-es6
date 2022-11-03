0bd8c67d73e6111e591c1647a11c6d14acf7b0b9
Michael J. Radwin <michael@radwin.org>
2022-11-03 13:50:09 -0700

    chore: update deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,8 +2,10 @@ import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
-import pkg from './package.json';
-import {terser} from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
+import {createRequire} from 'node:module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
 
 const banner = '/*! ' + pkg.name + ' v' + pkg.version + ' */';
 
