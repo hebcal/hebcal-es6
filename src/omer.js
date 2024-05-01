3f4f3f5dd068b17d141b21fb64c1b4aece03b069
Michael J. Radwin <michael@radwin.org>
2024-04-30 22:16:41 -0700

    Fix dagesh and nikud on Omer

--- src/omer.js
+++ src/omer.js
@@ -1,28 +1,6 @@
-import {gematriya} from '@hebcal/hdate';
+import {gematriya, omerSefira, omerTodayIs, omerEmoji} from '@hebcal/hdate';
 import {Event, flags} from './event.js';
 import {Locale} from './locale.js';
-
-const sefirot = [
-  null,
-  'Lovingkindness',
-  'Might',
-  'Beauty',
-  'Eternity',
-  'Splendor',
-  'Foundation',
-  'Majesty',
-];
-
-const sefirotTranslit = [
-  null,
-  'Chesed',
-  'Gevurah',
-  'Tiferet',
-  'Netzach',
-  'Hod',
-  'Yesod',
-  'Malkhut',
-];
 
 /** Represents a day 1-49 of counting the Omer from Pesach to Shavuot */
 export class OmerEvent extends Event {
@@ -44,39 +22,10 @@ export class OmerEvent extends Event {
    * @return {string}
    */
   sefira(lang='en') {
-    if (lang === 'he') {
-      return this.sefiraHe();
-    } else if (lang === 'translit') {
-      return this.sefiraTranslit();
-    } else {
-      const week = sefirot[this.weekNumber];
-      const dayWithinWeek = sefirot[this.daysWithinWeeks];
-      return `${dayWithinWeek} within ${week}`;
+    if (lang !== 'he' && lang !== 'translit') {
+      lang = 'en';
     }
-  }
-  /**
-   * @private
-   * @return {string}
-   */
-  sefiraTranslit() {
-    const weekNum = this.weekNumber;
-    const translitWeek = sefirotTranslit[weekNum];
-    const translitDayWithinWeek = sefirotTranslit[this.daysWithinWeeks];
-    const translitPrefix = weekNum === 2 || weekNum === 6 ? 'shebi' : `sheb'`;
-    return `${translitDayWithinWeek} ${translitPrefix}${translitWeek}`;
-  }
-  /**
-   * @private
-   * @return {string}
-   */
-  sefiraHe() {
-    const weekNum = this.weekNumber;
-    const week = sefirot[weekNum];
-    const dayWithinWeek = sefirot[this.daysWithinWeeks];
-    const heWeek = Locale.gettext(week, 'he');
-    const heDayWithinWeek = Locale.gettext(dayWithinWeek, 'he');
-    const hePrefix = weekNum === 2 || weekNum === 6 ? 'שֶׁבִּ' : 'שֶׁבְּ';
-    return `${heDayWithinWeek} ${hePrefix}${heWeek}`.normalize();
+    return omerSefira(this.omer, lang);
   }
   /**
    * @todo use gettext()
@@ -100,16 +49,7 @@ export class OmerEvent extends Event {
   /** @return {string} */
   getEmoji() {
     if (typeof this.emoji === 'string') return this.emoji;
-    const number = this.omer;
-    if (number <= 20) {
-      return String.fromCodePoint(9312 + number - 1);
-    } else if (number <= 35) {
-      // between 21 and 35 inclusive
-      return String.fromCodePoint(12881 + number - 21);
-    } else {
-      // between 36 and 49 inclusive
-      return String.fromCodePoint(12977 + number - 36);
-    }
+    return omerEmoji(this.omer);
   }
   /** @return {number} */
   getWeeks() {
@@ -130,108 +70,15 @@ export class OmerEvent extends Event {
       locale = locale.toLowerCase();
     }
     if (locale === 'he') {
-      return getTodayIsHe(this.omer);
+      return omerTodayIs(this.omer, 'he');
     } else if (locale === 'he-x-nonikud') {
-      return Locale.hebrewStripNikkud(getTodayIsHe(this.omer));
+      const str = omerTodayIs(this.omer, 'he');
+      return Locale.hebrewStripNikkud(str);
     }
-    const totalDaysStr = (this.omer === 1) ? 'day' : 'days';
-    let str = `Today is ${this.omer} ${totalDaysStr}`;
-    if (this.weekNumber > 1 || this.omer === 7) {
-      const day7 = this.daysWithinWeeks === 7;
-      const numWeeks = day7 ? this.weekNumber : this.weekNumber - 1;
-      const weeksStr = (numWeeks === 1) ? 'week' : 'weeks';
-      str += `, which is ${numWeeks} ${weeksStr}`;
-      if (!day7) {
-        const daysStr = (this.daysWithinWeeks === 1) ? 'day' : 'days';
-        str += ` and ${this.daysWithinWeeks} ${daysStr}`;
-      }
-    }
-    return str + ' of the Omer';
+    return omerTodayIs(this.omer, 'en');
   }
   /** @return {string} */
   url() {
     return `https://www.hebcal.com/omer/${this.getDate().getFullYear()}/${this.omer}`;
   }
-}
-
-// adapted from pip hdate package (GPL)
-// https://github.com/py-libhdate/py-libhdate/blob/master/hdate/date.py
-
-const tens = ['', 'עֲשָׂרָה', 'עֶשְׂרִים', 'שְׁלוֹשִׁים', 'אַרְבָּעִים'];
-const ones = [
-  '',
-  'אֶחָד',
-  'שְׁנַיִם',
-  'שְׁלוֹשָׁה',
-  'אַרְבָּעָה',
-  'חֲמִשָׁה',
-  'שִׁשָׁה',
-  'שִׁבְעָה',
-  'שְׁמוֹנָה',
-  'תִּשְׁעָה',
-];
-
-const shnei = 'שְׁנֵי';
-const yamim = 'יָמִים';
-const shneiYamim = shnei + ' ' + yamim;
-const shavuot = 'שָׁבוּעוֹת';
-const yom = 'יוֹם';
-const yomEchad = yom + ' ' + ones[1];
-
-/**
- * @private
- * @param {number} omer
- * @return {string}
- */
-function getTodayIsHe(omer) {
-  const ten = Math.floor(omer / 10);
-  const one = omer % 10;
-  let str = 'הַיוֹם ';
-  if (10 < omer && omer < 20) {
-    str += ones[one] + ' עָשָׂר';
-  } else if (omer > 9) {
-    str += ones[one];
-    if (one) {
-      str += ' וְ';
-    }
-  }
-  if (omer > 2) {
-    if ((omer > 20) || (omer === 10) || (omer === 20)) {
-      str += tens[ten];
-    }
-    if (omer < 11) {
-      str += ones[one] + ' ' + yamim + ' ';
-    } else {
-      str += ' ' + yom + ' ';
-    }
-  } else if (omer === 1) {
-    str += yomEchad + ' ';
-  } else { // omer == 2
-    str += shneiYamim + ' ';
-  }
-  if (omer > 6) {
-    str = str.trim(); // remove trailing space before comma
-    str += ', שְׁהֵם ';
-    const weeks = Math.floor(omer / 7);
-    const days = omer % 7;
-    if (weeks > 2) {
-      str += ones[weeks] + ' ' + shavuot + ' ';
-    } else if (weeks == 1) {
-      str += 'שָׁבוּעַ' + ' ' + ones[1] + ' ';
-    } else { // weeks == 2
-      str += shnei + ' ' + shavuot + ' ';
-    }
-    if (days) {
-      str += 'וְ';
-      if (days > 2) {
-        str += ones[days] + ' ' + yamim + ' ';
-      } else if (days == 1) {
-        str += yomEchad + ' ';
-      } else { // days == 2
-        str += shneiYamim + ' ';
-      }
-    }
-  }
-  str += 'לָעוֹמֶר';
-  return str.normalize();
 }
