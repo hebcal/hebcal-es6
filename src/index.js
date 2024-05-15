62c5a307b589b237182b4ffa60237e27dda4f306
Yisroel Yakovson <yyakovson613@gmail.com>
2024-05-15 15:09:25 +0300

    expose holidayDescs

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-export {gematriya, gematriyaStrToNum} from '@hebcal/hdate';
-export {Locale} from './locale.js';
-export {greg, months} from '@hebcal/hdate';
-export {HDate} from './hdate.js';
-export {HebrewDateEvent} from './HebrewDateEvent.js';
-export {Event, flags} from './event.js';
-export {GeoLocation, NOAACalculator} from '@hebcal/noaa';
-export {Location} from './location.js';
-export {Zmanim} from './zmanim.js';
-export {TimedEvent, CandleLightingEvent, HavdalahEvent} from './candles.js';
-export {Molad, MoladEvent} from './molad.js';
-export {OmerEvent} from './omer.js';
-export {Sedra, parshiot} from './sedra.js';
-export {ParshaEvent} from './ParshaEvent.js';
-export {HolidayEvent, AsaraBTevetEvent, MevarchimChodeshEvent,
-  RoshChodeshEvent, RoshHashanaEvent} from './holidays.js';
-export {DailyLearning} from './DailyLearning.js';
-export {HebrewCalendar} from './hebcal.js';
-export {version} from './pkgVersion.js';
+export { gematriya, gematriyaStrToNum } from '@hebcal/hdate';
+export { Locale } from './locale.js';
+export { greg, months } from '@hebcal/hdate';
+export { HDate } from './hdate.js';
+export { HebrewDateEvent } from './HebrewDateEvent.js';
+export { Event, flags } from './event.js';
+export { GeoLocation, NOAACalculator } from '@hebcal/noaa';
+export { Location } from './location.js';
+export { Zmanim } from './zmanim.js';
+export { TimedEvent, CandleLightingEvent, HavdalahEvent } from './candles.js';
+export { Molad, MoladEvent } from './molad.js';
+export { OmerEvent } from './omer.js';
+export { Sedra, parshiot } from './sedra.js';
+export { ParshaEvent } from './ParshaEvent.js';
+export {
+  HolidayEvent,
+  AsaraBTevetEvent,
+  MevarchimChodeshEvent,
+  RoshChodeshEvent,
+  RoshHashanaEvent,
+} from './holidays.js';
+export { DailyLearning } from './DailyLearning.js';
+export { HebrewCalendar } from './hebcal.js';
+export { holidayDescs as holidayDesc } from './holidayDescs.js';
+export { version } from './pkgVersion.js';
