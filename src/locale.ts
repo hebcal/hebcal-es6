52fc276ff33eb27fc9dd6ae7004ee4285d3a5848
Michael J. Radwin <michael@radwin.org>
2025-09-03 19:46:41 -0700

    Use Locale.copyLocaleNoNikud to avoid code duplication

--- src/locale.ts
+++ src/locale.ts
@@ -1,4 +1,4 @@
-import {Locale, LocaleData, StringArrayMap} from '@hebcal/hdate';
+import {Locale} from '@hebcal/hdate';
 import poAshkenazi from './ashkenazi.po';
 import poHe from './he.po';
 import noNikudOverride from './he-x-NoNikud.po';
@@ -11,14 +11,6 @@ Locale.addTranslations('ashkenazi', poAshkenazi);
 Locale.addTranslations('a', poAshkenazi);
 
 /* Hebrew without nikkud */
-const heStrs = poHe.contexts[''];
-const heNoNikud: StringArrayMap = {};
-for (const [key, val] of Object.entries(heStrs)) {
-  heNoNikud[key] = [Locale.hebrewStripNikkud(val[0])];
-}
-const poHeNoNikud: LocaleData = {
-  headers: poHe.headers,
-  contexts: {'': heNoNikud},
-} as const;
+const poHeNoNikud = Locale.copyLocaleNoNikud(poHe);
 Locale.addTranslations('he-x-NoNikud', poHeNoNikud);
 Locale.addTranslations('he-x-NoNikud', noNikudOverride);
