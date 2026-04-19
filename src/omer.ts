17fcbf7feb48fe218ae32f9c548c83838f912e38
Michael J. Radwin <michael@radwin.org>
2026-04-19 11:20:55 -0400

    don't generate OmerEvent urls for Hebrew year 4000 and earlier

--- src/omer.ts
+++ src/omer.ts
@@ -316,7 +316,7 @@ export class OmerEvent extends Event {
 
   url(): string | undefined {
     const year = this.getDate().getFullYear();
-    if (year < 3860 || year > 6759) {
+    if (year <= 4000 || year > 6759) {
       return undefined;
     }
     return `https://www.hebcal.com/omer/${year}/${this.omer}`;
